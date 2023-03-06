42f5ef75c82f13ba926639817dd4020e9954e323
29bianca <biancavale2003@gmail.com>
2023-03-06 12:53:48 +0000

    alinea a

--- TPC3/tpc3.py
+++ TPC3/tpc3.py
@@ -4,11 +4,26 @@ def proc_file():
     with open('processos.txt', 'r') as f:
         linhas = f.readlines()
 
-        pattern = re.compiler(r"^(?P<Pasta>\d+)::(?P<Data>\d{4}\-\d{2}\-\d{2})?::(?P<Nome>[a-zA-Z \,\.\(\)]+)?::(?P<Pai>[a-zA-Z \,\.\(\)]+)?::(?P<Mae>[a-zA-Z \,\.\(\)]+)?::(?P<Observacoes>(\s*.*\s*)*)?::$")
+        pattern = re.compile(r"^(?P<Pasta>\d+)::(?P<Data>\d{4}\-\d{2}\-\d{2})?::(?P<Nome>[a-zA-Z \,\.\(\)]+)?::(?P<Pai>[a-zA-Z \,\.\(\)]+)?::(?P<Mae>[a-zA-Z \,\.\(\)]+)?::(?P<Observacoes>(\s*.*\s*)*)?::$")
         linhasProcess = []
 
     for linha in linhas:
         linhasProcess.append(pattern.match(linha).groupdict())
 
-    return linhasProcess    
+    return linhasProcess
 
+
+def proc_ano(linhasProcess):
+    dic = {}
+    pattern = re.compile(r"(\d{4})")
+
+    for linha in linhasProcess:
+        data = linha["Data"]
+        ano = pattern.match(data).group(1)
+        if ano in dic:
+            dic[ano] +=1
+        else:
+            dic[ano] = 1
+    return dic        
+
+print(proc_ano(proc_file()))
