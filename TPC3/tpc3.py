52dd0d18651cc78ff2466ab62cbc1ae9db16db46
29bianca <biancavale2003@gmail.com>
2023-03-07 00:09:25 +0000

    b

--- TPC3/tpc3.py
+++ TPC3/tpc3.py
@@ -1,3 +1,4 @@
+import math
 import re 
 
 def proc_file():
@@ -26,4 +27,41 @@ def proc_ano(linhasProcess):
             dic[ano] = 1
     return dic        
 
-print(proc_ano(proc_file()))
+#print(proc_ano(proc_file()))
+#print(proc_file())
+
+def freq_nomes(linhasProcess):
+    patternNome = re.compile(r"^(?P<nome>\w+)\b.*?\b((?P<sobrenome>\w+))?$")
+    patternAno = re.compile(r"(\d{4})")
+    dic = {}
+
+    for linha in linhasProcess:
+        data = linha["Data"]
+        ano = patternAno.match(data).group(1)
+        seculo = math.floor(int(ano) / 100) + 1
+        nomes = patternNome.search(linha["Nome"])
+        if seculo not in dic.keys():
+            dic[seculo] = {}
+        if nomes != None:
+            for nome in [nomes['nome'],nomes['sobrenome']]:
+                if nome not in dic[seculo].keys():
+                    dic[seculo][nome] = 1
+                else:
+                    dic[seculo][nome] += 1
+        
+
+    # Ordernar dicionário
+    for key,value in dic.items():
+        dic[key] = dict(sorted(value.items(), key=lambda x: x[1], reverse=True))
+
+    for key,value  in dic.items():
+        print("** TOP 5 [SEC " + str(key) + "] **")
+
+        for i, (key,value) in enumerate(dic[key].items()):
+            if (i>4):
+                break
+            print(str(i+1) + ". " + key + " (" +str(value)+")")
+        print("")
+    
+
+freq_nomes(proc_file())
